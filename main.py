3fa48f90c5c20b4bbf70217bd3a212bf6ab6c527
hex-plex <56990337+hex-plex@users.noreply.github.com>
2020-05-29 19:17:13 +0530

    Version 1.21
    Has Progress bar

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
 import sys
+import pytube
 from pytube import YouTube
-def main(url="",res=None):
-    if url=="":
-        url = input("Enter the URL of the youtube video: ")
-    if res!=None:
-        res=res[1:]
-    print("specify the resolution")
-    res=str(input())
-    try:
-        yt = YouTube(url)
-        print("\nTitle of Video:", yt.title)
-        if res!=None:
-            try:
-                stream = yt.streams.filter(progressive=True, file_extension='mp4',res=import sys
-from pytube import YouTube
-from pytube.cli import on_progress
-def main(url="",res=None):
-    if url=="":
-        url = input("Enter the URL of the youtube video: ")
-    if res!=None:
-        res=res[1:]
+from pytybe.cli import on_progress
+import urllib
+
+def getRequest(url: str):
     try:
         yt = YouTube(url,on_progress_callback=on_progress)
         print("\nTitle of Video:", yt.title)
-        if res!=None:
-            try:
-                stream = yt.streams.filter(progressive=True, file_extension='mp4',res=str(res))
-            except:
-                print("the specified resoulution is not found")
-        else:
-            stream = yt.streams.filter(progressive=True, file_extension='mp4')
-        stream = stream.order_by('resolution')
-        stream = stream.desc()
-        stream = stream.first()
-        print(stream)
-        stream = stream.download()
-        print("Done Downloading", stream)
-    
+    except urllib.error.URLError:
+        print("\nNo Internet. Please make sure you have an active internet connection.")
+        exit()
+    except pytube.exceptions.RegexMatchError:
+        print("\nInvalid address for youtube video. ")
+        exit()
     except:
-        print("\nConnection Error\n")
-        print("TRY AGAIN LATER!")
+        print("\nSome unknown error occured. Please report it at https://github.com/shubhanshu02/Youtube-Downloader/issues")
+        exit()
+    return yt
+
+def AvailableRes(yt):
+    AvailableResolutions = set()
+    for i in yt.streams.filter(progressive=True):
+        if type(i.resolution)==str:
+            AvailableResolutions.add(i.resolution)
+    print("\nAvailable resolutions:", *AvailableResolutions)
+
+def main(url="",res=None):
+    if url=="":
+        url = input("Enter the URL of the youtube video: ")
+
+    yt = getRequest(url)
+    AvailableRes(yt)
+    
+    if res != None:
+        res=res[1:]
+    else:
+        res = str(input("Specify the resolution: "))
+    
+    stream = yt.streams.filter(progressive=True, file_extension='mp4',res=str(res)).first()
+
+    if (stream == None):
+        print("\nResolution Not Found")
+        exit()
+        
+    print("\nStream connected. Stream:\n", stream)
+    print("\nStarting Download...")
+    stream = stream.download()
+    print("\nDone Downloading\nFile Location:", stream)
+
 
 if  __name__=="__main__":
     if len(sys.argv)==3:
@@ -49,31 +58,4 @@ if  __name__=="__main__":
     elif len(sys.argv)==1:
         main()
     else :
-        print("Wrong arguement please input:")
-        print("python main.py -res url")
-str(res))
-            except:
-                print("the specified resoulution is not found")
-        else:
-            stream = yt.streams.filter(progressive=True, file_extension='mp4')
-        stream = stream.order_by('resolution')
-        stream = stream.desc()
-        stream = stream.first()
-        print(stream)
-        stream = stream.download()
-        print("Done Downloading", stream)
-    
-    except:
-        print("\nConnection Error\n")
-        print("TRY AGAIN LATER!")
-
-if  __name__=="__main__":
-    if len(sys.argv)==3:
-        main(sys.argv[2],sys.argv[1])
-    elif len(sys.argv)==2:
-        main(sys.argv[1])
-    elif len(sys.argv)==1:
-        main()
-    else :
-        print("Wrong arguement please input:")
-        print("python main.py -res url")
+        print("You have entered the wrong format for arguments. Please input in the form:\npython main.py -resolution url")
